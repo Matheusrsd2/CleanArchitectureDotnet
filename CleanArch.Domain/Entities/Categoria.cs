597c0a9d2a6d7376f4bff9f14445a8bbc281c427
matheusrsd2 <matttheus.reee@gmail.com>
2022-03-09 14:44:46 -0300

    Initial commit

---
+++ CleanArch.Domain/Entities/Categoria.cs
@@ -0,0 +1,37 @@
+﻿using CleanArch.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Categoria
+    {
+        public Categoria(string nome)
+        {
+            ValidarDominio(nome);
+        }
+
+        public Categoria(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public ICollection<Produto> Produtos { get; set; }
+
+        private void ValidarDominio(string nome)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome),
+                "Nome Inválido. O nome é obnrigatório");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome),
+                "Nome Inválido. Necessário ao menos 3 caracteres");
+
+            Nome = nome;
+        }
+    }
+}
---
+++ CleanArch.Domain/Entities/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Produto
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+        public int Estoque { get; private set; }
+        public string Imagem { get; private set; }
+        public int CategoriaId { get; private set; }
+        public Categoria Categoria { get; private set; }
+    }
+}
